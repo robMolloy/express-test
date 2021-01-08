a93a0034f0b1b42bae11f10dcd33ba6dcee0e024
robMolloy <robmolloy@hotmail.co.uk>
2021-01-08 23:59:32 +0000

    Contact us form validation

--- src/components/tailored/sections/ContactUsForm.js
+++ src/components/tailored/sections/ContactUsForm.js
@@ -3,7 +3,6 @@ import Input from "../../generic/fields/Input";
 
 import SectionLightBg from "../../generic/sections/SectionLightBg";
 import Container from "../../generic/containers/Container";
-import Text from "../../generic/text/Text";
 import Title from "../../generic/text/TitleUnderline";
 import GridContainer from "../../generic/grids/GridContainer";
 import GridItem from "../../generic/grids/GridItem";
@@ -48,10 +47,13 @@ const ContactUsForm = () => {
   const [success, setSuccess] = React.useState(false);
   const [formValues, setFormValues] = React.useState({});
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    setFormValues(form.getValues());
-    setSuccess(true);
+    const result = await form.trigger("lastName");
+    if (result) {
+      setFormValues(form.getValues());
+      setSuccess(true);
+    }
   };
 
   const getFormProps = ({ name }) => {
@@ -66,15 +68,14 @@ const ContactUsForm = () => {
   return (
     <SectionLightBg>
       <Container>
-        <Title>Contact Us</Title>
         <br />
-        <br />
+
         {success && (
           <>
-            <Text align="center">
+            <Title size="2">
               This Form doesn't actually do anything, but here's your data in a
               table.
-            </Text>
+            </Title>
             <br />
             <GridContainer spacing={2}>
               {Object.entries(formValues).map(([label, value]) => {
@@ -93,65 +94,74 @@ const ContactUsForm = () => {
           </>
         )}
         {!success && (
-          <form noValidate onSubmit={async (e) => onSubmit(e)}>
-            <GridContainer spacing={2}>
-              <GridItem xs={12} sm={6}>
-                <Input
-                  label="First Name *"
-                  {...getFormProps({ name: "con_first_name" })}
-                />
-              </GridItem>
-              <GridItem xs={12} sm={6}>
-                <Input
-                  label="Last Name *"
-                  {...getFormProps({ name: "con_last_name" })}
-                />
-              </GridItem>
-              <GridItem xs={12}>
-                <Input
-                  label="Email Address *"
-                  helperText="Email used for the booking"
-                  {...getFormProps({ name: "con_email" })}
-                />
-              </GridItem>
-              <GridItem xs={12}>
-                <Input
-                  label="Confirm Email Address *"
-                  {...getFormProps({ name: "con_email_confirm" })}
-                />
-              </GridItem>
-              <GridItem xs={12}>
-                <Input
-                  label="Mobile Number *"
-                  {...getFormProps({ name: "con_phone" })}
-                />
-              </GridItem>
-              <GridItem xs={12}>
-                <Input
-                  label="Confirm Mobile Number *"
-                  {...getFormProps({ name: "con_phone_confirm" })}
-                />
-              </GridItem>
-              <GridItem xs={12}>
-                <Input
-                  label="Express Test Booking Reference *"
-                  placeholder="Booking reference for primary person (will begin with 'CPT')."
-                  {...getFormProps({ name: "con_booking_ref" })}
-                />
-              </GridItem>
-              <GridItem xs={12}>
-                <Input
-                  label="Please share any more details that are relevant to your case"
-                  {...getFormProps({ name: "con_details" })}
-                />
-              </GridItem>
-              <GridItem xs={12}>
-                <ButtonRow
-                  array={[<ButtonPrimary type="submit">Submit</ButtonPrimary>]}
-                />
-              </GridItem>
-            </GridContainer>
-          </form>
+          <>
+            <Title size="2">
+              If your question hasn't been answered, please fill out the form
+              below
+            </Title>
+            <br />
+            <form noValidate onSubmit={async (e) => onSubmit(e)}>
+              <GridContainer spacing={2}>
+                <GridItem xs={12} sm={6}>
+                  <Input
+                    label="First Name *"
+                    {...getFormProps({ name: "con_first_name" })}
+                  />
+                </GridItem>
+                <GridItem xs={12} sm={6}>
+                  <Input
+                    label="Last Name *"
+                    {...getFormProps({ name: "con_last_name" })}
+                  />
+                </GridItem>
+                <GridItem xs={12}>
+                  <Input
+                    label="Email Address *"
+                    helperText="Email used for the booking"
+                    {...getFormProps({ name: "con_email" })}
+                  />
+                </GridItem>
+                <GridItem xs={12}>
+                  <Input
+                    label="Confirm Email Address *"
+                    {...getFormProps({ name: "con_email_confirm" })}
+                  />
+                </GridItem>
+                <GridItem xs={12}>
+                  <Input
+                    label="Mobile Number *"
+                    {...getFormProps({ name: "con_phone" })}
+                  />
+                </GridItem>
+                <GridItem xs={12}>
+                  <Input
+                    label="Confirm Mobile Number *"
+                    {...getFormProps({ name: "con_phone_confirm" })}
+                  />
+                </GridItem>
+                <GridItem xs={12}>
+                  <Input
+                    label="Express Test Booking Reference *"
+                    placeholder="Booking reference for primary person (will begin with 'CPT')."
+                    {...getFormProps({ name: "con_booking_ref" })}
+                  />
+                </GridItem>
+                <GridItem xs={12}>
+                  <Input
+                    label="Please share any more details that are relevant to your case"
+                    {...getFormProps({ name: "con_details" })}
+                  />
+                </GridItem>
+                <GridItem xs={12}>
+                  <ButtonRow
+                    array={[
+                      <ButtonPrimary type="submit">Submit</ButtonPrimary>,
+                    ]}
+                  />
+                </GridItem>
+              </GridContainer>
+            </form>
+          </>
         )}
         <br />
       </Container>
