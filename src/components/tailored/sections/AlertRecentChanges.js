9176bfa851f97c19329e00c2b3c0e6332e384fb1
robMolloy <robmolloy@hotmail.co.uk>
2021-01-09 00:12:21 +0000

    AlertRecentChanges updated

--- src/components/tailored/sections/AlertRecentChanges.js
+++ src/components/tailored/sections/AlertRecentChanges.js
@@ -1,27 +1,15 @@
 import React from "react";
 
 import SectionAlert from "../../generic/sections/SectionAlert";
-import Text from "../../generic/text/Text";
+import Title from "../../generic/text/Title";
 
 const AlertRecentChanges = () => {
   return (
     <SectionAlert>
       <div>
-        <Text align="center">
-          If you have been impacted by the recent Government changes to Covid
-          restrictions and wish to cancel your booked test, please follow this
-          link:{" "}
-          <a href="https://support.expresstest.co.uk/">
-            Booking Cancellation / Voucher Request.
-          </a>
-          <br />
-          <br />
-          This will advise you of our cancellation policy and the process for
-          submitting your cancellation and requesting your voucher.
-          <br />
-          <br />
-          Thank you for choosing ExpressTest.
-        </Text>
+        <Title align="center" size="1">
+          Please note – there is no HGV access to our Testing Facilities
+        </Title>
       </div>
     </SectionAlert>
   );
