44d860c14ad81b86d97df63ac4abfb519dd7968b
robMolloy <robmolloy@hotmail.co.uk>
2021-01-11 11:28:36 +0000

    Sidebar generic

--- src/components/tailored/bars/Drawer.js
+++ src/components/tailored/bars/Drawer.js
@@ -1,46 +1,25 @@
 import React from "react";
 
-import MUIDrawer from "@material-ui/core/Drawer";
-import makeStyles from "@material-ui/core/styles/makeStyles";
-import MUIDivider from "@material-ui/core/Divider";
+// import MUIDrawer from "@material-ui/core/Drawer";
+// import makeStyles from "@material-ui/core/styles/makeStyles";
+// import MUIDivider from "@material-ui/core/Divider";
 
-import allListItems from "./allListItems";
-import NavLinkList from "./NavLinkList";
+// import allListItems from "./allListItems";
+// import config from "../../../config";
+// import NavLinkList from "./NavLinkList";
+import Sidebar from "../../generic/navigation/Sidebar";
 
-const getGroupedListItems = () => {
-  let rtn = [];
+// const getGroupedListItems = () => {
+//   let rtn = [];
 
-  allListItems.forEach((listItem) => (rtn[listItem.order] = []));
-  allListItems.forEach((listItem) => rtn[listItem.order].push(listItem));
+//   config.pages.forEach((pageData) => (rtn[pageData.order] = []));
+//   config.pages.forEach((pageData) => rtn[pageData.order].push(pageData));
 
-  return rtn;
-};
+//   return rtn;
+// };
 
 const Drawer = (props = {}) => {
-  let side, state, toggleDrawer;
-  ({ side = "left", state, toggleDrawer, ...props } = props);
-
-  const classes = makeStyles({ list: { width: 250 } })();
-
-  const groupedListItems = getGroupedListItems();
-
-  return (
-    <MUIDrawer anchor={side} open={state} onClose={toggleDrawer}>
-      <div
-        className={classes.list}
-        onClick={toggleDrawer}
-        onKeyDown={toggleDrawer}
-        {...props}
-      >
-        {groupedListItems.map((listItemGroup, j) => (
-          <React.Fragment key={j}>
-            <NavLinkList {...{ toggleDrawer }} array={listItemGroup} />
-            {j === groupedListItems.length - 1 ? "" : <MUIDivider />}
-          </React.Fragment>
-        ))}
-      </div>
-    </MUIDrawer>
-  );
+  return <Sidebar {...props} />;
 };
 
 export default Drawer;
