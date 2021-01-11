237d9731e975858b9ffb24e3971d49fcc3d23b03
robMolloy <robmolloy@hotmail.co.uk>
2021-01-11 11:29:42 +0000

    Drawer uses only the generic sidebar

--- src/components/tailored/bars/Drawer.js
+++ src/components/tailored/bars/Drawer.js
@@ -1,22 +1,6 @@
 import React from "react";
 
-// import MUIDrawer from "@material-ui/core/Drawer";
-// import makeStyles from "@material-ui/core/styles/makeStyles";
-// import MUIDivider from "@material-ui/core/Divider";
-
-// import allListItems from "./allListItems";
-// import config from "../../../config";
-// import NavLinkList from "./NavLinkList";
 import Sidebar from "../../generic/navigation/Sidebar";
-
-// const getGroupedListItems = () => {
-//   let rtn = [];
-
-//   config.pages.forEach((pageData) => (rtn[pageData.order] = []));
-//   config.pages.forEach((pageData) => rtn[pageData.order].push(pageData));
-
-//   return rtn;
-// };
 
 const Drawer = (props = {}) => {
   return <Sidebar {...props} />;
